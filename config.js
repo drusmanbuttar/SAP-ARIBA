702bdc67a34f458b5b1e9a7b436679771b283309
drusmanbuttar <dr.usmanbuttar@gmail.com>
2026-09-08 12:51:06 +0500

    Configure Google Sheets API endpoint

---
+++ config.js
@@ -0,0 +1,5 @@
+// Google Apps Script Web App endpoint for the worksheet.
+// Replace this URL if you deploy a new Apps Script version.
+const APP_CONFIG = {
+  apiUrl: 'https://script.google.com/macros/s/AKfycbzVcxFM47az82nsgdcyVa2_qXEmvzKChTXtl5ZYptrNqUiFihyrPLlocO7jTp5-HTCvzQ/exec'
+};
